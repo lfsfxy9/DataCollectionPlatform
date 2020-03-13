b4627af3ac15bfc2172c58b14b721008df0c12ce
jizg <jizg6688@foxmail.com>
2020-03-13 20:58:29 +0800

    update

--- src/test/java/com/tfjybj/dcp/DataCollectionPlatformApplicationTests.java
+++ src/test/java/com/tfjybj/dcp/DataCollectionPlatformApplicationTests.java
@@ -1,13 +1,13 @@
 package com.tfjybj.dcp;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DataCollectionPlatformApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
