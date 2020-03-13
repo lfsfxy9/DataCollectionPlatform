c8fe871ffd840a0356c71fe40613703f4d6d3596
lfsfxy9 <jizg6688@foxmail.com>
2020-03-13 18:27:28 +0800

    解决提示找不到servlet的问题；使用本地tomcat

--- src/main/java/com/tfjybj/dcp/crawlers/Basic.java
+++ src/main/java/com/tfjybj/dcp/crawlers/Basic.java
@@ -8,22 +8,38 @@ import org.seimicrawler.xpath.JXDocument;
 
 import java.util.List;
 
-/**
+/**  SeimiCrawler 的爬虫框架入口，用来实现具体爬去数据的任务管理
+ *
  * Created by zg on 2020/3/2.
  */
 @Crawler(name = "basic")
 public class Basic extends BaseSeimiCrawler {
+    /**
+     * @description 用来标记要爬取数据的url地址
+     * @author jizg
+     * @param
+     * @updateTime 2020/3/13 18:08:00
+     * @return
+     * @throws
+     */
     @Override
     public String[] startUrls() {
-        //两个是测试去重的
-        return new String[]{"http://www.cnblogs.com/","http://www.cnblogs.com/"};
+        return new String[]{"http://www.chaoxing.com/","https://mooc1-1.chaoxing.com/work/getAllWork?classId=14910088&courseId=207434064&isdisplaytable=2&mooc=1&ut=t&enc=eb9a9c828ebd4f8b129c83b2b4bb658e&cpi=123274613&openc=65d00e69846767fa4a03b0497b19deb4"};
     }
 
+    /**
+     * @description 处理StartUrl返回的请求数据
+     * @author jizg
+     * @param
+     * @updateTime 2020/3/13 18:08:40
+     * @return
+     * @throws
+     */
     @Override
     public void start(Response response) {
         JXDocument doc = response.document();
         try {
-            List<Object> urls = doc.sel("//a[@class='titlelnk']/@href");
+            List<Object> urls = doc.sel("//a[@class='searchMenu clearfix']");
             logger.info("{}", urls.size());
             for (Object s:urls){
                 push(Request.build(s.toString(), Basic::getTitle));
@@ -32,6 +48,15 @@ public class Basic extends BaseSeimiCrawler {
             e.printStackTrace();
         }
     }
+
+    /**
+     * @description
+     * @author jizg
+     * @param
+     * @updateTime 2020/3/13 18:10:20
+     * @return
+     * @throws
+     */
     public void getTitle(Response response){
         JXDocument doc = response.document();
         try {
